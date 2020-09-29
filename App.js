6242c5968c0001d124ae6f6da3c94ece2378ed2e
Lunghekani <lunghivlanga@gmail.com>
2020-09-30 01:08:20 +0200

    initial commit
    reupload of the folder

---
+++ App.js
@@ -0,0 +1,17 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+//import BarcodeScan from "./app/assets/screens/BarcodeScan.js"; //uncomment to run
+
+export default function App() {
+  // return <BarcodeScan />; // uncomment to run
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
